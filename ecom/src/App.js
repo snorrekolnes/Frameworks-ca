f7f0ca72e84c838d4c2390b7d062b47754c7ac2c
snorre kolnes røgenes <snorre.kolnes@hotmail.com>
2023-04-11 19:19:04 +0200

    ca

---
+++ ecom/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/shared/Header";
+//import {useSelector} from "react-redux";
+
+
+function App() {
+ // const {isLoading} = useSelector(state => state.loader)
+  return (
+<>
+<Header/>
+
+
+
+</>
+  );
+}
+
+export default App;
